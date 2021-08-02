d401b90829cb5bdee8b6398deaa3780c94eebdc8
lulu4747 <ludogriph@gmail.com>
2021-08-03 00:25:12 +0200

    work in progress

--- d06/ex00/Literal.cpp
+++ d06/ex00/Literal.cpp
@@ -5,23 +5,12 @@
 #include <stdexcept>
 #include "Literal.hpp"
 
-Literal::Literal(void){
+Literal::Literal(std::string const & src):_unset(false){
 
-	return ;
-}
-
-Literal::Literal(Literal const & src){
-
-	*this = src;
-	return ;
-}
-
-Literal::Literal(std::string const & src){
-
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		if ((this->*_isType[i])(src) == true)
-			i = 5;
+			break;
 	}
 	return ;
 }
@@ -29,17 +18,6 @@ Literal::Literal(std::string const & src){
 Literal::~Literal(void){
 
 	return ;
-}
-
-Literal &	Literal::operator=(Literal const & rhs){
-
-	if (this != &rhs)
-	{
-		this->_c = rhs.getChar();
-		this->_i = rhs.getInt();
-		this->_f = rhs.getFloat();
-		this->_d = rhs.getDouble();
-	}
 }
 
 char	Literal::getChar(void) const{
@@ -67,46 +45,84 @@ bool	Literal::isPseudo(std::string const & src){
 	if (src.size() <= 5)
 	{
 		if (!src.compare("+inff"))
-			Literal::CastAll(this->_f = std::numeric_limits<float>::infinity());
+			Literal::CastAll(this->_f = Literal::inff);
 
 		else if (!src.compare("-inff"))
-			Literal::CastAll(this->_f = -(std::numeric_limits<float>::infinity()));
+			Literal::CastAll(this->_f = -Literal::inff);
 
 		else if (!src.compare("nanf"))
-			Literal::CastAll(this->_f = std::numeric_limits<float>::quiet_NaN());
+			Literal::CastAll(this->_f = Literal::nanf);
 
 		else if (!src.compare("+inf"))
-			Literal::CastAll(this->_d = std::numeric_limits<double>::infinity());
+			Literal::CastAll(this->_d = Literal::inf);
 
 		else if (!src.compare("-inf"))
-			Literal::CastAll(this->_d = -(std::numeric_limits<double>::infinity()));
+			Literal::CastAll(this->_d = -Literal::inf);
 
 		else if (!src.compare("nan"))
-			Literal::CastAll(this->_d = std::numeric_limits<double>::quiet_NaN());
+			Literal::CastAll(this->_d = Literal::nan);
 	}
 	return false;
 }
 
-bool	Literal::isChar(std::string const & src){}
+bool	Literal::isChar(std::string const & src){
 
-bool	Literal::isInt(std::string const & src){}
+	if (src.size() == 1 && std::isprint(src.at(0)))
+	{
+		Literal::CastAll(this->_c = src.at(0));
+		return true;
+	}
+	return false
+}
+
+bool	Literal::isInt(std::string const & src){
+
+	for (int i = 0; i < src.size(); i++)
+	{
+		if (!std::isdigit(src.at(i)))
+			return false;
+	}
+}
 
 bool	Literal::isFloat(std::string const & src){}
 
 bool	Literal::isDouble(std::string const & src){}
 
-void	Literal::CastAll(char c){
-//pense except
-	this->_i = static_cast<int>(c);
+bool	Literal::isUnset(std::string const & src){
+
+	return this->_unset = true;
 }
 
-void	Literal::CastAll(int i){}
+void	Literal::CastAll(char c){
+
+	this->_i = static_cast<int>(c);
+	this->_f = static_cast<float>(i);
+	this->_d = static_cast<double>(i);
+	return ;
+}
+
+void	Literal::CastAll(int i){
+
+	this->_c = static_cast<char>(i);
+	this->_f = static_cast<float>(i);
+	this->_d = static_cast<double>(i);
+	return ;
+}
 
 void	Literal::CastAll(float f){
 
-
+	this->_c = static_cast<char>(f);
+	this->_i = static_cast<int>(f);
+	this->_d = static_cast<double>(f);
+	return ;
 }
 
-void	Literal::CastAll(double d){}
+void	Literal::CastAll(double d){
+
+	this->_c = static_cast<char>(d);
+	this->_i = static_cast<int>(d);
+	this->_f = static_cast<float>(d);
+	return ;
+}
 
 std::ostream &	operator<<(std::ostream & o, Literal const & rhs){}
--- d06/ex00/Literal.hpp
+++ d06/ex00/Literal.hpp
@@ -7,12 +7,9 @@ class Literal{
 
 public:
 
-	Literal(void);
-	Literal(Literal const & src);
 	Literal(std::string const & src);
 	virtual	~Literal(void);
 
-	virtual Literal &	operator=(Literal const & rhs);
 
 	char	getChar(void) const;
 	int		getInt(void) const;
@@ -21,32 +18,44 @@ public:
 
 private:
 
+	Literal(void);
+	Literal(Literal const & src);
+	virtual Literal &	operator=(Literal const & rhs);
+
 	char	_c;
 	int		_i;
 	float	_f;
 	double	_d;
+	bool	_unset;
 
 	bool	isPseudo(std::string const & src);
 	bool	isChar(std::string const & src);
 	bool	isInt(std::string const & src);
 	bool	isFloat(std::string const & src);
 	bool	isDouble(std::string const & src);
+	bool	isUnset(std::string const & src);
 
 	typedef	bool	(Literal::*Type)(std::string const &);
 	
-	Type const	_isType[5] = 
+	Type const	_isType[6] = 
 	{
 		&Literal::isPseudo,
 		&Literal::isChar,
 		&Literal::isInt,
 		&Literal::isFloat,
-		&Literal::isDouble
+		&Literal::isDouble,
+		&Literal::isUnset
 	};
 
 	void	CastAll(char c);
 	void	CastAll(int i);
 	void	CastAll(float f);
 	void	CastAll(double d);
+
+	static float const	inff = std::numeric_limits<float>::infinity();
+	static float const	nanf = std::numeric_limits<float>::quiet_NaN();
+	static double const	inf = std::numeric_limits<double>::infinity();
+	static double const	nan = std::numeric_limits<double>::quiet_NaN();
 
 };
 
