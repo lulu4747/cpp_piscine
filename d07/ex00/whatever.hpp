ae099fae8767a8119aab9fe9a2586210007c92a1
lfourage <ludogriph@gmail.com>
2021-08-09 14:28:04 +0200

    template functions

---
+++ d07/ex00/whatever.hpp
@@ -0,0 +1,25 @@
+template< typename T >
+void	swap(T & x, T & y){
+
+	T	tmp = x;
+
+	x = y;
+	y = tmp;
+	return ;
+}
+
+template< typename T >
+T	min(T const & x, T const & y){
+
+	if (x < y)
+		return x;
+	return y;
+}
+
+template< typename T >
+T	max(T const & x, T const & y){
+
+	if (x > y)
+		return x;
+	return y;
+}
