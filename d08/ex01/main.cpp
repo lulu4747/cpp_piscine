b908b332e83aab6aa61221065b26b6008d60f5cf
lulu4747 <ludogriph@gmail.com>
2021-08-12 19:20:54 +0200

    more torough test

--- d08/ex01/main.cpp
+++ d08/ex01/main.cpp
@@ -14,4 +14,40 @@ int main()
 
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+
+	try
+	{
+		sp.addNumber(7);	
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "addNumber impossible (Span full)" << std::endl;
+	}
+
+	Span	sp2 = Span(10);
+
+	sp2.addNumber(8);
+	sp2.addNumber(-9);
+	sp2.addNumber(5);
+	sp2.addNumber(0);
+	sp2.addNumber(987);
+	sp2.addNumber(41);
+	sp2.addNumber(66);
+	sp2.addNumber(87);
+	sp2.addNumber(7);
+	sp2.addNumber(-9);
+
+	try
+	{
+		sp = sp2;	
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "impossible assignation from sp2" << std::endl;
+	}
+
+	std::cout << sp2.shortestSpan() << std::endl;
+	std::cout << sp2.longestSpan() << std::endl;
+
+	return 0;
 }
