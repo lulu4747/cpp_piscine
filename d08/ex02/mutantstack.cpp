be8b796b01d99268e9e0fee1dbe437f9b436533f
lulu4747 <ludogriph@gmail.com>
2021-08-13 15:09:45 +0200

    had to leave

---
+++ d08/ex02/mutantstack.cpp
@@ -0,0 +1,3 @@
+#include <stack>
+#include <algorithm>
+#include "mutantstack.hpp"
--- d08/ex02/mutantstack.hpp
+++ d08/ex02/mutantstack.hpp
@@ -1,4 +1,14 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+# include <stack>
+
+template< typename T >
+class MutantStack : public std::stack<T>{
+
+public:
+
+	std::iterator
+};
+
 #endif
