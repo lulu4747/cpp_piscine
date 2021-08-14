3b3803677c33bb6b91efc10a541b22500a5a4294
lulu4747 <ludogriph@gmail.com>
2021-08-14 12:00:39 +0200

    presentation

--- d08/ex02/mutantstack.hpp
+++ d08/ex02/mutantstack.hpp
@@ -19,17 +19,19 @@ public:
 
 	MutantStack &	operator=(MutantStack const & rhs){return *this = std::stack<T, Container>::operator=(rhs);}
 
-
 	typedef	typename Container::iterator		iterator;
 	typedef	typename Container::const_iterator	const_iterator;
 
 	iterator		begin(void){return std::stack<T, Container>::c.begin();}
-	iterator		end(void){return std::stack<T, Container>::c.end();}
-	iterator		rbegin(void){return std::stack<T, Container>::c.rbegin();}
-	iterator		rend(void){return std::stack<T, Container>::c.rend();}
 	const_iterator	begin(void) const{return std::stack<T, Container>::c.begin();}
+
+	iterator		end(void){return std::stack<T, Container>::c.end();}
 	const_iterator	end(void) const{return std::stack<T, Container>::c.end();}
+
+	iterator		rbegin(void){return std::stack<T, Container>::c.rbegin();}
 	const_iterator	rbegin(void) const{return std::stack<T, Container>::c.rbegin();}
+
+	iterator		rend(void){return std::stack<T, Container>::c.rend();}
 	const_iterator	rend(void) const{return std::stack<T, Container>::c.rend();}
 
 };
