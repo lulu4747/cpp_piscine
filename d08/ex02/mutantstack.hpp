12836972590f61cddd9251c138dfd310a63e7520
lulu4747 <ludogriph@gmail.com>
2021-08-14 11:59:17 +0200

    really working this time

--- d08/ex02/mutantstack.hpp
+++ d08/ex02/mutantstack.hpp
@@ -11,26 +11,26 @@ class MutantStack : public std::stack<T, Container>{
 public:
 
 	MutantStack(void): std::stack<T, Container>(){}
+
+	MutantStack(std::stack<T, Container> const & src): std::stack<T, Container>(src){}
 	MutantStack(MutantStack const & src): std::stack<T, Container>(src){}
+
 	virtual ~MutantStack(void){}
 
 	MutantStack &	operator=(MutantStack const & rhs){return *this = std::stack<T, Container>::operator=(rhs);}
 
+
 	typedef	typename Container::iterator		iterator;
 	typedef	typename Container::const_iterator	const_iterator;
 
-	iterator		begin(void){return this->_container.begin();}
-	iterator		end(void){return this->_container.end();}
-	iterator		rbegin(void){return this->_container.rbegin();}
-	iterator		rend(void){return this->_container.rend();}
-	const_iterator	begin(void) const{return this->_container.begin();}
-	const_iterator	end(void) const{return this->_container.end();}
-	const_iterator	rbegin(void) const{return this->_container.rbegin();}
-	const_iterator	rend(void) const{return this->_container.rend();}
-
-private:
-
-	Container		_container;
+	iterator		begin(void){return std::stack<T, Container>::c.begin();}
+	iterator		end(void){return std::stack<T, Container>::c.end();}
+	iterator		rbegin(void){return std::stack<T, Container>::c.rbegin();}
+	iterator		rend(void){return std::stack<T, Container>::c.rend();}
+	const_iterator	begin(void) const{return std::stack<T, Container>::c.begin();}
+	const_iterator	end(void) const{return std::stack<T, Container>::c.end();}
+	const_iterator	rbegin(void) const{return std::stack<T, Container>::c.rbegin();}
+	const_iterator	rend(void) const{return std::stack<T, Container>::c.rend();}
 
 };
 
